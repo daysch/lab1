d666944e33a63098e333c407a28b1a11f92c4ca3
Davey Schoenberg <dschoenberg@college.harvard.edu>
2018-02-02 00:18:20 -0500

    exercise 11

--- lab1.ml
+++ lab1.ml
@@ -1,3 +1,4 @@
+open List ;;
 (*
 			      CS51 Lab 1
 		     Basic Functional Programming
@@ -315,7 +316,7 @@ Exercise 11: Reimplement sum using fold_left, naming it sum_ho (for
 ......................................................................*)
 
 let sum_ho (lst : int list) : int =
-  failwith "sum_ho not implemented" ;;
+  fold_left (+) 0 lst ;;
 
 (*......................................................................
 Exercise 12: Reimplement prods using map.
